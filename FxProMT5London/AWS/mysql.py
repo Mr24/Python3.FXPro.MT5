62beaaebbdeb2a4c44d99eed94bafc954e7b7a85
Yoshio.Mr24 <sys.verysvery+github@gmail.com>
2021-01-13 21:29:29 +0900

    FXPro.Py3.MT5 : FxPro.AWS.RDS.MySQL.Simple.MT5.Price.Def - Ver.0.0.5 SetUp
    Changes to be committed:
    	modified:   FxProMT5London/AWS/mysql.py
    	modified:   FxProMT5London/main.py

--- FxProMT5London/AWS/mysql.py
+++ FxProMT5London/AWS/mysql.py
@@ -5,7 +5,7 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|       "VsV.Py3.FxPro.MT5.mysql.py - Ver.0.0.4 Update:2020.12.23" |
+#//|       "VsV.Py3.FxPro.MT5.mysql.py - Ver.0.0.5 Update:2021.01.13" |
 #//+------------------------------------------------------------------+
 import json
 
@@ -17,11 +17,8 @@ with open("AWS/aws_config.json", encoding='utf-8') as f:
 
 ### AWS.RDS.MySQL : Setup ###
 ENDPOINT = rds_dict['host'] 	# AWS.RDS.MySQL : EndPoint
-PORT = rds_dict['port']		# AWS.RDS.MySQL : Port
-USR = rds_dict['user']		# AWS.RDS.MySQL : UserName
-PW = rds_dict['password']			# AWS.RDS.MySQL : PassWord
+PORT = rds_dict['port']			# AWS.RDS.MySQL : Port
+USR = rds_dict['user']			# AWS.RDS.MySQL : UserName
+PW = rds_dict['password']		# AWS.RDS.MySQL : PassWord
 REGION = rds_dict['region']		# AWS.RDS.MySQL : Region
-DBNAME = rds_dict['database']		# AWS.RDS.MySQL : Database Name
-
-
-
+DBNAME = rds_dict['database']	# AWS.RDS.MySQL : Database Name
--- FxProMT5London/main.py
+++ FxProMT5London/main.py
@@ -5,7 +5,7 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|        "VsV.Py3.FxPro.MT5.main.py - Ver.0.0.4 Update:2020.12.23" |
+#//|        "VsV.Py3.FxPro.MT5.main.py - Ver.0.0.5 Update:2021.01.13" |
 #//+------------------------------------------------------------------+
 #//|                                   PyCharm : PlugIn - AWS ToolKit |
 #//|                               https://aws.amazon.com/jp/pycharm/ |
@@ -14,6 +14,8 @@ import json
 import os
 
 import MetaTrader5 as mt5
+from datetime import datetime
+
 import boto3 as b3
 import mysql.connector as mConn
 
@@ -49,7 +51,7 @@ except Exception as e:
     print("Database connection failed due to {}".format(e))
 
 
-### Demo.Session : Setup ###
+### MT5.Demo.Session : Setup ###
 DemoID = fxproSession.DemoID
 DemoPW = fxproSession.DemoPW
 FxServer = fxproSession.FxServer
@@ -64,5 +66,18 @@ print(mt5.terminal_info())
 ## MT5.Session : Version
 print(mt5.version())
 
+
+### MT5.Ticks : USDJPY - 1000
+usdjpy_ticks = mt5.copy_ticks_from("USDJPY", datetime(2020,12,23,9), 1000, mt5.COPY_TICKS_ALL)
+usdjpy_rates = mt5.copy_rates_from("USDJPY", mt5.TIMEFRAME_M1, datetime(2020,12,23,9), 1000)
+
+
 ## MT5.Session : ShutDown
 mt5.shutdown()
+
+
+# print('usdjpy_ticks(', len(usdjpy_ticks), ')')
+# for val in usdjpy_ticks[:10]: print(val)
+
+print('usdjpy_rates(', len(usdjpy_rates), ')')
+for val in usdjpy_rates[:10]: print(val)
